48ad4a27174f9eea23973666b433d3916436f818
ybutola <yogibutola@icloud.com>
2019-09-18 08:12:23 -0500

    Added a method to find the item by name.

--- src/main/java/com/butola/producer/repo/ItemRepository.java
+++ src/main/java/com/butola/producer/repo/ItemRepository.java
@@ -1,14 +1,18 @@
 package com.butola.producer.repo;
 
 import com.butola.producer.data.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ItemRepository extends CrudRepository<Item, Long>{
+import java.util.Optional;
 
-   // @Query("UPDATE Item c SET c.itemDescription = :itemDescription WHERE c.itemid = :itemid")
-   // int updateItem(@Param("itemid") Long itemid, @Param("itemDescription") String itemDescription);
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    /*@Query("UPDATE Item c SET c.itemDescription = ?2 WHERE c.itemid = ?1")
+    int updateItem(Long itemid, String itemDescription);*/
+
+    @Query("select i from Item i where i.itemName = ?1")
+    Optional<Item> findByItemName(String itemName);
 }
