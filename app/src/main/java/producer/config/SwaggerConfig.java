0a12cfccef3460db0f6bc7ae60c144e9074becea
ybutola <yogibutola@icloud.com>
2019-11-16 20:31:23 -0600

    Fixed swagger documentation.

--- app/src/main/java/producer/config/SwaggerConfig.java
+++ app/src/main/java/producer/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package producer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,8 +21,18 @@ public class SwaggerConfig {
     public Docket restProducerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.butola.producer.controllers"))
-                .paths(PathSelectors.regex("/restproducer.*"))
+                .apis(RequestHandlerSelectors.basePackage("producer.controllers"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Rest Producer API")
+                .contact(new Contact("Yogi Butola", "javahotpot.blogspot.com", "yogibutola@icloud.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
                 .build();
     }
 }
